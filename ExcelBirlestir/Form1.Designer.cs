07e98db964220dc862863fe9e6a890aad4600e3d
teStroniX <ayaz.abdurrahim@gmail.com>
2020-08-29 14:57:13 +0300

    Add project files.

---
+++ ExcelBirlestir/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ExcelBirlestir
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(403, 140);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(421, 64);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 140);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Dosyaları Seç";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 210);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(484, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Çıktı Al";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(395, 12);
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(101, 20);
+			this.numericUpDown1.TabIndex = 4;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(309, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Sütun Sayısı:";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 9);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(291, 49);
+			this.progressBar1.TabIndex = 6;
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(395, 38);
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(101, 20);
+			this.numericUpDown2.TabIndex = 4;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(309, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Başlangıç Satır:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(508, 245);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Excel Birleştir";
+			this.TopMost = true;
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ ExcelBirlestir/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelBirlestir
+{
+	public partial class Form1 : Form
+	{
+		bool go = true;
+		string name = "";
+		string[] files;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public void Output()
+		{
+			int x = 1, y = Convert.ToInt32(numericUpDown2.Value) - 1;
+			int[] toplamlar = new int[Convert.ToInt32(numericUpDown1.Value)];
+
+			for (int i = 0; i < numericUpDown1.Value - 1; i++)
+			{
+				toplamlar[i] = 0;
+			}
+
+			object Missing = System.Reflection.Missing.Value;
+			Excel.Application ExcelUygulama = new Excel.Application();
+			Excel.Workbook ExcelProje = ExcelUygulama.Workbooks.Add(files[0]);
+			Excel.Worksheet ExcelSayfa = (Excel.Worksheet)ExcelProje.Worksheets.get_Item(1);
+			Excel.Range ExcelRange = ExcelSayfa.UsedRange;
+			ExcelSayfa = (Excel.Worksheet)ExcelUygulama.ActiveSheet;
+			ExcelUygulama.Visible = false;
+
+			for (int i = 0; i < files.Length; i++)
+			{
+				Excel.Application ExcelUygulama2 = new Excel.Application();
+				Excel.Workbook ExcelProje2 = ExcelUygulama.Workbooks.Add(files[i]);
+				Excel.Worksheet ExcelSayfa2 = (Excel.Worksheet)ExcelProje.Worksheets.get_Item(1);
+				Excel.Range ExcelRange2 = ExcelSayfa.UsedRange;
+				ExcelSayfa2 = (Excel.Worksheet)ExcelUygulama.Sheets[1];
+
+				for (int j = 3; go; j++)
+				{
+					ExcelRange2 = (Excel.Range)ExcelSayfa2.Cells[j, 1];
+					if (ExcelRange2.Value2 == "TOPLAM")
+						break;
+					y++;
+					for (int k = 1; k <= numericUpDown1.Value; k++)
+					{
+						ExcelRange2 = (Excel.Range)ExcelSayfa2.Cells[j, k];
+						ExcelRange = (Excel.Range)ExcelSayfa.Cells[y, k];
+						ExcelRange.ClearFormats();
+						if (k == 1)
+						{
+							ExcelRange.Interior.Color = Color.FromArgb(123, 123, 123);
+							ExcelRange.RowHeight = 40;
+							ExcelRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+							ExcelRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+						}
+						else
+						{
+							try { toplamlar[k - 2] += ExcelRange2.Value2 == null ? 0 : ExcelRange2.Value2; } catch { }
+						}
+						ExcelRange.Borders.Color = Color.Black;
+						ExcelRange.Value = ExcelRange2.Value2;
+					}
+				}
+
+				ExcelRange2 = null;
+				ExcelSayfa2 = null;
+				ExcelProje2.Close(false);
+				ExcelUygulama2.Quit();
+				ExcelProje2 = null;
+				ExcelUygulama2 = null;
+			}
+			y++;
+			for (int i = 1; i <= numericUpDown1.Value; i++)
+			{
+				ExcelRange = (Excel.Range)ExcelSayfa.Cells[y, i];
+				if (i == 1)
+				{
+					ExcelRange.Interior.Color = Color.FromArgb(123, 123, 123);
+					ExcelRange.RowHeight = 40;
+					ExcelRange.Value2 = "TOPLAM";
+					ExcelRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+					ExcelRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+				}
+				else
+				{
+					ExcelRange.Value2 = toplamlar[i - 2];
+				}
+				ExcelRange.Borders.Color = Color.Black;
+			}
+
+			//ExcelSayfa.Columns.AutoFit();
+			//ExcelSayfa.Rows.AutoFit();
+
+			ExcelProje.SaveAs(name, Excel.XlFileFormat.xlWorkbookDefault, Missing, Missing, false, Missing, Excel.XlSaveAsAccessMode.xlNoChange);
+			ExcelProje.Close(true, Missing, Missing);
+			ExcelUygulama.Quit();
+			MessageBox.Show("Tamamlandı!");
+			progressBar1.Invoke(new MethodInvoker(() =>
+			{
+				progressBar1.Style = ProgressBarStyle.Blocks;
+			}));
+
+			foreach (var process in Process.GetProcessesByName("EXCEL"))
+			{
+				process.Kill();
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Multiselect = true;
+			var result = dialog.ShowDialog();
+
+			if(result == DialogResult.OK)
+			{
+				for(int i=0; i< dialog.FileNames.Length; i++)
+					richTextBox1.AppendText(dialog.FileNames[i] + (i==dialog.FileNames.Length - 1 ? "" : "\n"));
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.Filter = "Excel Dosyası | .xlsx";
+
+			var result = dialog.ShowDialog();
+
+			if (result == DialogResult.OK)
+			{
+				name = dialog.FileName;
+				files = richTextBox1.Lines;
+				progressBar1.Style = ProgressBarStyle.Marquee;
+				BackgroundWorker worker = new BackgroundWorker();
+				worker.DoWork += DoWork;
+				worker.RunWorkerAsync();
+			}
+		}
+		public void DoWork(object obj, EventArgs e)
+		{
+			Output();
+		}
+	}
+}
